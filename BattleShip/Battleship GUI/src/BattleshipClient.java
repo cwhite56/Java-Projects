cca000624d3633bd6f555756954c06dec788d878
CWhite56 <cam.white1117@gmail.com>
2025-12-30 13:04:47 -0500

    Client side input finished

--- BattleShip/Battleship GUI/src/BattleshipClient.java
+++ BattleShip/Battleship GUI/src/BattleshipClient.java
@@ -6,38 +6,50 @@ import java.net.*;
 public class BattleshipClient {
 	private PrintWriter out;
 	private BufferedReader in;
+	private Socket socket;
 	private Player player;
 	private BattleshipGUI gui;
+	private static boolean gameFinished = false;
 
 	public BattleshipClient() throws IOException{
 		player = new Player();
 		gui = new BattleshipGUI(player, this);
-		setupNetworking();
+		
 	
 	}
 
 	public static void main(String[] args) throws IOException {
 		BattleshipClient client = new BattleshipClient();
+		while (!gameFinished) {
+
+		}
+		client.closeSocket();
 	}
 
 	/**
 	 * Method to initialize connection to server
 	 * @throws IOException
 	 */
-	private void setupNetworking() throws IOException{
-	Socket socket = new Socket("localhost", 5000);
-	PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-	BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+	public void setupNetworking() throws IOException{
+	socket = new Socket("localhost", 5000);
+	out = new PrintWriter(socket.getOutputStream(), true);
+	in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+	OutputStream outputStream = socket.getOutputStream();
+	ObjectOutputStream objectOutputStream = new ObjectOutputStream(outputStream); 
 
-	// need way to send ship list to server
-	socket.close();
+	objectOutputStream.writeObject(player.getShipList());
+	
 	}
 	public boolean sendData() throws IOException{
-		//out.println(player.getPlayerGuess());
-		System.out.println(player.getPlayerGuess());
 
-		//return Boolean.parseBoolean(in.readLine());
-		return false;
+		out.println(player.getPlayerGuess());
+
+		return Boolean.parseBoolean(in.readLine());
+		
+	}
+
+	public void closeSocket() throws IOException {
+		this.socket.close();
 	}
 	
 }
